c0fe1b4b7bdb39019e388472e5595497532edf19
Thiraput01 <Thiraputball@gmail.com>
2024-12-16 01:05:46 +0700

    update ui for follower-predict

--- app.py
+++ app.py
@@ -16,31 +16,23 @@ scaler = joblib.load('follow_predict/scaler.pkl')  # Load the scaler that was us
 def user_input():
     # Slider inputs for each feature with unique keys
     impressions = st.sidebar.slider('Impressions', min_value=0, max_value=50000, value=25000, key='impressions')
-    from_home = st.sidebar.slider('From Home', min_value=0, max_value=20000, value=10000, key='from_home')
-    from_hashtags = st.sidebar.slider('From Hashtags', min_value=0, max_value=20000, value=10000, key='from_hashtags')
-    from_explore = st.sidebar.slider('From Explore', min_value=0, max_value=20000, value=10000, key='from_explore')
-    from_other = st.sidebar.slider('From Other', min_value=0, max_value=5000, value=2500, key='from_other')
-    saves = st.sidebar.slider('Saves', min_value=0, max_value=2000, value=1000, key='saves')
+    saves = st.sidebar.slider('Saves', min_value=0, max_value=1500, value=750, key='saves')
     shares = st.sidebar.slider('Shares', min_value=0, max_value=100, value=50, key='shares')
     likes = st.sidebar.slider('Likes', min_value=0, max_value=1000, value=500, key='likes')
-    profile_visits = st.sidebar.slider('Profile Visits', min_value=0, max_value=500, value=250, key='profile_visits')
+    profile_visits = st.sidebar.slider('Profile Visits', min_value=0, max_value=1000, value=500, key='profile_visits')
 
     # Display user inputs
     st.write("## Selected Input Values:")
     st.write(f"Impressions: {impressions}")
-    st.write(f"From Home: {from_home}")
-    st.write(f"From Hashtags: {from_hashtags}")
-    st.write(f"From Explore: {from_explore}")
-    st.write(f"From Other: {from_other}")
     st.write(f"Saves: {saves}")
     st.write(f"Shares: {shares}")
     st.write(f"Likes: {likes}")
     st.write(f"Profile Visits: {profile_visits}")
-    return impressions, from_home, from_hashtags, from_explore, from_other, saves, shares, likes, profile_visits
+    return impressions, saves, shares, likes, profile_visits
 
-def model_output(impressions, from_home, from_hashtags, from_explore, from_other, saves, shares, likes, profile_visits):
+def model_output(impressions, saves, shares, likes, profile_visits):
     # Prepare the input data
-    input_data = [[impressions, from_home, from_hashtags, from_explore, from_other, saves, shares, likes, profile_visits]]
+    input_data = [[impressions, saves, shares, likes, profile_visits]]
     
     # Normalize the input data using the scaler
     input_scaled = scaler.transform(input_data)  # Use the scaler that was used during training
@@ -53,11 +45,12 @@ def model_output(impressions, from_home, from_hashtags, from_explore, from_other
 
 
 # Get user input
-impressions, from_home, from_hashtags, from_explore, from_other, saves, shares, likes, profile_visits = user_input()
+impressions, saves, shares, likes, profile_visits = user_input()
 
 # Button to trigger prediction
 if st.button('Predict'):
-    output = model_output(impressions, from_home, from_hashtags, from_explore, from_other, saves, shares, likes, profile_visits)
-    st.write(f"Predicted Follow Count: {output[0]}")
+    output = model_output(impressions, saves, shares, likes, profile_visits)
+    output = int(output)
+    st.write(f"Predicted Follow Count: {output}")
 else:
     st.write("Click the 'Predict' button to get the prediction.")
